a5c20df8ad53da8fa8cb9ab853aa62ecdec15ef6
cburkey <cburkey@entermediasoftware.com>
2011-07-18 10:16:38 -0400

    Michael tweaks

--- etc/install.js
+++ etc/install.js
@@ -5,7 +5,7 @@ importPackage( Packages.java.io );
 importPackage( Packages.com.openedit.modules.update );
 importPackage( Packages.com.openedit.modules.scheduler );
 
-var war = "http://dev.openedit.org/anthill/projects/openedit-db/ROOT.war";
+var war = "http://dev.entermediasoftware.com/jenkins/job/entermedia-sql/lastSuccessfulBuild/artifact/deploy/ROOT.war";
 
 var root = moduleManager.getBean("root").getAbsolutePath();
 var web = root + "/WEB-INF";
