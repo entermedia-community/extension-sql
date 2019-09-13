80fe50e095f8258d233b661b5cd7930ce0f3995a
Thomas N <t.nadalie33@gmail.com>
2019-09-13 12:17:04 -0500

    Fix installer

--- etc/install.js
+++ etc/install.js
@@ -5,7 +5,9 @@ importPackage( Packages.java.io );
 importPackage( Packages.com.openedit.modules.update );
 importPackage( Packages.com.openedit.modules.scheduler );
 
-var war = "http://dev.entermediasoftware.com/jenkins/job/entermedia-sql/lastSuccessfulBuild/artifact/deploy/ROOT.war";
+var name = "extension-sql";
+
+var war = "http://dev.entermediasoftware.com/jenkins/job/@BRANCH@" + name + "/lastSuccessfulBuild/artifact/deploy/" + name + ".zip";
 
 var root = moduleManager.getBean("root").getAbsolutePath();
 var web = root + "/WEB-INF";
@@ -13,41 +15,24 @@ var tmp = web + "/tmp";
 
 log.info("1. GET THE LATEST WAR FILE");
 var downloader = new Downloader();
-downloader.download( war, tmp + "/ROOT.war");
+downloader.download( war, tmp + "/" + name + ".zip");
 
 log.info("2. UNZIP WAR FILE");
 var unziper = new ZipUtil();
 unziper.unzip(  tmp + "/ROOT.war",  tmp );
+unziper.unzip(  tmp + "/" + name + ".zip",  tmp );
 
 log.info("3. REPLACE LIBS");
 var files = new FileUtils();
 
-files.deleteMatch( web + "/lib/openedit-db*.jar");
-files.copyFileByMatch( tmp + "/WEB-INF/lib/openedit-db*.jar", web + "/lib/");
+files.deleteMatch( web + "/lib/@BRANCH@" + name + "*.jar");
+files.deleteMatch( web + "/lib/commons-dbcp-*.jar");
+files.deleteMatch( web + "/lib/mysql-connector-java-*.jar");
 
-files.deleteMatch( web + "/lib/antlr*.jar");
-files.copyFileByMatch( tmp + "/WEB-INF/lib/antlr*.jar", web + "/lib/");
+files.copyFileByMatch( tmp + "/lib/*.jar", web + "/lib/");
 
-files.deleteMatch( web + "/lib/asm*.jar");
-files.copyFileByMatch( tmp + "/WEB-INF/lib/asm*.jar", web + "/lib/");
-
-files.deleteMatch( web + "/lib/cglib*.jar");
-files.copyFileByMatch( tmp + "/WEB-INF/lib/cglib*.jar", web + "/lib/");
-
-files.deleteMatch( web + "/lib/hibernate*.jar");
-files.copyFileByMatch( tmp + "/WEB-INF/lib/hibernate*.jar", web + "/lib/");
-
-files.deleteMatch( web + "/lib/mysql-connector*.jar");
-files.copyFileByMatch( tmp + "/WEB-INF/lib/mysql-connector*.jar", web + "/lib/");
-
-
-//log.info("4. UPGRADE BASE DIR");
-//files.deleteAll( root + "/base/tracker");
-//files.deleteAll( root + "/WEB-INF/base/tracker");
-//files.copyFiles( tmp + "/WEB-INF/base/tracker", root + "/WEB-INF/base/tracker");
-
-log.info("5. CLEAN UP");
+log.info("4. CLEAN UP");
 files.deleteAll(tmp);
 
-log.info("6. DB UPDATE COMPLETED");
+log.info("5. UPGRADE COMPLETED");
 
